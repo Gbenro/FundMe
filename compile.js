13ff468153d7b46f67f05908660016282ac85f65
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-11-26 20:05:41 -0600

    Added compile and compiles successfully

--- compile.js
+++ compile.js
@@ -11,7 +11,6 @@ const output = solc.compile(source, 1).contracts
 
 fs.ensureDirSync(buildPath)
 
-
 for (let contract in output) {
   fs.outputJsonSync(
     path.resolve(buildPath, contract.replace(':', '') + '.json'),
