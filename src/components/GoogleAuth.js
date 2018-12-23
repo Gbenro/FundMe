1bb831e63a2866e7ec429bb1ad221dc16e6542fa
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-23 14:09:47 -0600

    editing Auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn, signOut } from '../actions'
 
 class GoogleAuth extends Component {
-  state = {
-    isSignedIn: null
-  }
   componentDidMount () {
     window.gapi.load('client:auth2', () => {
       window.gapi.client
@@ -14,14 +13,18 @@ class GoogleAuth extends Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance()
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+          this.onAuthChange(this.auth.isSignedIn.get())
           this.auth.isSignedIn.listen(this.onAuthChange)
         })
     })
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn()
+    } else {
+      this.props.signOut()
+    }
   }
 
   onSignInClick = () => {
@@ -31,9 +34,9 @@ class GoogleAuth extends Component {
     this.auth.signOut()
   }
   renderAuthButton () {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className='ui red google button'>
           <i className='google icon' />
@@ -53,5 +56,13 @@ class GoogleAuth extends Component {
     return <div> {this.renderAuthButton()}</div>
   }
 }
+const mapStateToProps = state => {
+  return {
+    isSignedIn: state.auth.isSignedIn
+  }
+}
 
-export default GoogleAuth
+export default connect(
+  null,
+  { signIn,signOut }
+)(GoogleAuth)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 
 import App from './components/App'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
-
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
   document.querySelector('.container')
